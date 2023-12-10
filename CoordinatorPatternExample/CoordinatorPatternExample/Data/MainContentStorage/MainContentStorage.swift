2d58c9fca39a6c807575ff5943d2f581ff041275
yjjem <marksim0606@gmail.com>
2023-12-10 21:44:31 +0900

    feat: add some functionalities to MainContentStorage

--- CoordinatorPatternExample/CoordinatorPatternExample/Data/MainContentStorage/MainContentStorage.swift
+++ CoordinatorPatternExample/CoordinatorPatternExample/Data/MainContentStorage/MainContentStorage.swift
@@ -7,9 +7,27 @@
 
 final class MainContentStorage {
     
+    // MARK: Property(s)
+    
     private(set) var contentList: [MainContent] = []
+    
+    // MARK: Function(s)
     
     func fetchMainContentList() {
         contentList = (1...80).map { MainContent(number: $0) }
+    }
+    
+    func filterContentList(minValue: Int, maxValue: Int) {
+        let filterRange = minValue...maxValue
+        print(minValue)
+        resetContentList()
+        contentList = contentList.filter { filterRange.contains($0.number) }
+        print(contentList)
+    }
+    
+    // MARK: Private Function(s)
+    
+    private func resetContentList() {
+        fetchMainContentList()
     }
 }
