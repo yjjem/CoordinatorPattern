ba46a3ed7b482daecbc938cff285daf906a6f824
yjjem <marksim0606@gmail.com>
2023-12-12 12:57:18 +0900

    feat: add tabController configuration

--- CoordinatorPatternExample/CoordinatorPatternExample/Scene/Main/TabCoordinator.swift
+++ CoordinatorPatternExample/CoordinatorPatternExample/Scene/Main/TabCoordinator.swift
@@ -3,7 +3,7 @@
 //  CoordinatorPatternExample
 //
 //  Copyright (c) 2023 Jeremy All rights reserved.
-    
+
 
 import UIKit
 
@@ -14,9 +14,29 @@ final class TabCoordinator: Coordinator {
     var childCoordinators: [UUID : Coordinator] = [:]
     let identifier: UUID = UUID()
     
+    private let navigationController: UINavigationController
+    private let tabBarController: UITabBarController = {
+        let tabController = UITabBarController()
+        return tabController
+    }()
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
     // MARK: Function(s)
     
     func start() {
+        configureTabController()
+        navigationController.setViewControllers([tabBarController], animated: true)
+    }
+    
+    // MARK: Private Function(s)
+        
+    private func configureTabController() {
+        let tabViewControllers: [UIViewController] = []
+        tabBarController.setViewControllers(tabViewControllers, animated: true)
+    }
         
     }
 }
