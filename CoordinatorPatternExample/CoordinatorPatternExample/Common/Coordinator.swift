fee002c9a1163365f64670ad009457b31f802345
yjjem <marksim0606@gmail.com>
2023-12-14 20:47:34 +0900

    refactor: make Coordinator protocol conform to AnyObject

--- CoordinatorPatternExample/CoordinatorPatternExample/Common/Coordinator.swift
+++ CoordinatorPatternExample/CoordinatorPatternExample/Common/Coordinator.swift
@@ -4,9 +4,10 @@
 //
 //  Copyright (c) 2023 Jeremy All rights reserved.
 
+
 import UIKit
 
-protocol Coordinator {
+protocol Coordinator: AnyObject {
     
     var childCoordinators: [UUID: Coordinator] { get set }
     var identifier: UUID  { get }
