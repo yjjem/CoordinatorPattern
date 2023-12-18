8b7485816ea2b400966ae4edef18c7eb22973d2c
yjjem <marksim0606@gmail.com>
2023-12-18 19:32:18 +0900

    feat: implement FlowResponder

---
+++ CoordinatorPatternExample/CoordinatorPatternExample/Common/FlowResponder.swift
@@ -0,0 +1,25 @@
+//
+//  FlowResponder.swift
+//  CoordinatorPatternExample
+//
+//  Copyright (c) 2023 Jeremy All rights reserved.
+    
+
+import Foundation
+
+class FlowResponder: NSObject {
+    
+    var nextFlowResponder: FlowResponder?
+    
+    func handleFlowEvent(_ selector: Selector) {
+        if responds(to: selector) {
+            perform(selector)
+        } else {
+            nextFlowResponder?.handleFlowEvent(selector)
+        }
+    }
+    
+    func setNext(flowResponder: FlowResponder) {
+        self.nextFlowResponder = flowResponder
+    }
+}
