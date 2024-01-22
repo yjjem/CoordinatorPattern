ff41507e8bd0c57ff3062428d9e6e7b1bc2cf058
yjjem <marksim0606@gmail.com>
2024-01-23 00:17:54 +0900

    feat: add TabBarFlow

---
+++ CoordinatorPatternExample/CoordinatorPatternExample/Flows/TabBarFlow.swift
@@ -0,0 +1,42 @@
+//
+//  TabBarFlow.swift
+//  CoordinatorPatternExample
+//
+//  Copyright (c) 2024 Jeremy All rights reserved.
+    
+
+import UIKit
+import RxFlow
+
+final class TabBarFlow: Flow {
+    
+    var root: Presentable {
+        return tabBarController
+    }
+    
+    var initialStep: Step {
+        return FlowSteps.tabBar
+    }
+    
+    private lazy var tabBarController: UITabBarController = UITabBarController()
+    
+    func navigate(to step: Step) -> FlowContributors {
+        guard let step = step as? FlowSteps else { return .none }
+        
+        switch step {
+        case .tabBar:
+            return showTabBarFlow()
+        default: 
+            return .none
+        }
+    }
+    
+    // MARK: Private Function(s)
+    
+    private func showTabBarFlow() -> FlowContributors {
+        let numbersView = MainContentListViewController()
+        numbersView.tabBarItem = TabTypes.numbers.tabBarItem
+        tabBarController.setViewControllers([numbersView], animated: true)
+        return .one(flowContributor: .contribute(withNext: numbersView))
+    }
+}
